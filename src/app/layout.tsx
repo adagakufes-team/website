d0d96aae252d92fc5d5febeca1c4a289007b5841
YUKI <78786632+ykshio@users.noreply.github.com>
2026-04-15 18:30:55 +0900

    chore: Prettier, husky, lint-staged, GitHub Actions CIを導入

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,10 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="ja"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
+    <html lang="ja" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
