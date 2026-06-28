e7b980f40ea93d4650d123bf8578f6a8ba61ea1e
24aj009 <niku1529@icloud.com>
2026-06-28 16:13:11 +0900

    fix:同一ページの移動の最適化

--- src/components/archive.tsx
+++ src/components/archive.tsx
@@ -43,7 +43,6 @@ export default function ArchiveSection() {
               <FadeIn>
                 <Link
                   href="/archive"
-                  target="_blank"
                   rel="noopener noreferrer"
                   className="mt-10 flex w-full items-center justify-center gap-2 border-2 border-orange-400 bg-white/60 px-6 py-4 text-lg text-gray-900 shadow-sm transition hover:bg-orange-50 active:translate-y-1 md:mt-0 md:text-xl"
                 >
